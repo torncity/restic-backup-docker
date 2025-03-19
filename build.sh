c0482a43531902eb58bb9bfd397e5fc651ccfc13
Vedmaka <god.vedmaka@gmail.com>
2025-03-19 22:10:37 +0100

    Update build script to support multi-platform Docker images

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-docker build --rm -t restic-backup .
+IMAGE_TAG="ghcr.io/torncity/restic-backup-docker:stream"
+
+# https://docs.docker.com/build/building/multi-platform/#building-multi-platform-images
+docker buildx build \
+    --platform linux/amd64,linux/arm64 \
+    -t "$IMAGE_TAG" .
